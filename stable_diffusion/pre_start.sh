3b3b2757dde438f4db6d19cd8565bd3c7a8a9ccd
bluedragon1228 <joeyharvey066@gmail.com>
2023-07-30 17:46:09 +0200

    Start Tensorboard before downloading SDXL models

--- stable_diffusion/pre_start.sh
+++ stable_diffusion/pre_start.sh
@@ -69,12 +69,6 @@ else
     deactivate
 fi
 
-if [ ${INSTALL_SDXL} ]; then
-  echo "Beginning SDXL installation"
-  /install_sdxl.sh
-  echo "SDXL installation complete"
-fi
-
 if [ ${ENABLE_TENSORBOARD} ]; then
     echo "Starting Tensorboard"
     cd /workspace
@@ -89,3 +83,9 @@ if [ ${ENABLE_TENSORBOARD} ]; then
 fi
 
 echo "All services have been started"
+
+if [ ${INSTALL_SDXL} ]; then
+  echo "Beginning SDXL installation"
+  /install_sdxl.sh
+  echo "SDXL installation complete"
+fi
