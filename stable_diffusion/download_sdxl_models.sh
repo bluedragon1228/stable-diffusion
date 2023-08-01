5badf723acfc3c9360c6f2aaaf27e60143aa1db0
bluedragon1228 <joeyharvey066@gmail.com>
2023-08-01 14:53:49 +0200

    Include refiner extension and download SDXL models that include VAE

--- stable_diffusion/download_sdxl_models.sh
+++ stable_diffusion/download_sdxl_models.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-echo "Installing SDXL Base and Refiner Models"
+echo "Downloading SDXL Base and Refiner Models"
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
+wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0_0.9vae.safetensors
+wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0_0.9vae.safetensors
 
-echo "Installing SDXL Offset LoRA"
+echo "Downloading SDXL Offset LoRA"
 cd /workspace/stable-diffusion-webui/models/Lora
 wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors
-
-echo "Installing SDXL VAE"
-cd /workspace/stable-diffusion-webui/models/VAE
-wget https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors
