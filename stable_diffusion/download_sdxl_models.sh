78a81d36b4a7d3a7969b1ae65889c9044a1f2998
bluedragon1228 <joeyharvey066@gmail.com>
2023-08-11 13:26:08 +0200

    Revert SDXL VAE models to the non-VAE versions because apparently the VAE ones cause issues training Kohya_ss

--- stable_diffusion/download_sdxl_models.sh
+++ stable_diffusion/download_sdxl_models.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Downloading SDXL Base and Refiner Models"
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0_0.9vae.safetensors
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0_0.9vae.safetensors
+wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
+wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
 
 echo "Downloading SDXL Offset LoRA"
 cd /workspace/stable-diffusion-webui/models/Lora
--- stable_diffusion/pre_start.sh
+++ stable_diffusion/pre_start.sh
@@ -62,7 +62,6 @@ else
 
     echo "Starting Kohya_ss Web UI"
     cd /workspace/kohya_ss
-    source venv/bin/activate
     nohup ./gui.sh --listen 0.0.0.0 --server_port 3011 --headless > /workspace/logs/kohya_ss.log 2>&1 &
     echo "Kohya_ss started"
     echo "Log file: /workspace/logs/kohya_ss.log"
