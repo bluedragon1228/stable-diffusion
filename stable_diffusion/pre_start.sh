d53da61ee2b46fb69ff19785060d2339f7452327
bluedragon1228 <joeyharvey066@gmail.com>
2023-08-16 18:22:21 +0200

    Add ComfyUI and download SDXL models in the background

--- stable_diffusion/pre_start.sh
+++ stable_diffusion/pre_start.sh
@@ -18,12 +18,20 @@ echo "Syncing Kohya_ss to workspace, please wait..."
 rsync -au /kohya_ss/ /workspace/kohya_ss/
 rm -rf /kohya_ss
 
+# Sync ComfyUI to workspace to support Network volumes
+echo "Syncing ComfyUI to workspace, please wait..."
+rsync -au /ComfyUI/ /workspace/ComfyUI/
+rm -rf /ComfyUI
+
 # Fix the venvs to make them work from /workspace
 echo "Fixing Stable Diffusion Web UI venv..."
 /fix_venv.sh /venv /workspace/venv
 
 echo "Fixing Kohya_ss venv..."
 /fix_venv.sh /kohya_ss/venv /workspace/kohya_ss/venv
+
+echo "Fixing ComfyUI venv..."
+/fix_venv.sh /ComfyUI/venv /workspace/ComfyUI/venv
 
 # Link model and VAE
 ln -s /sd-models/v1-5-pruned.safetensors /workspace/stable-diffusion-webui/models/Stable-diffusion/v1-5-pruned.safetensors
@@ -50,8 +58,16 @@ then
     echo "   cd /workspace/kohya_ss"
     echo "   deactivate"
     echo "   ./gui.sh --listen 0.0.0.0 --server_port 3011 --headless"
+    echo ""
+    echo "   ComfyUI"
+    echo "   ---------------------------------------------"
+    echo "   cd /workspace/ComfyUI"
+    echo "   deactivate"
+    echo "   source venv/bin/activate"
+    echo "   python3 main.py --listen 0.0.0.0 --port 3021"
 else
     mkdir -p /workspace/logs
+
     echo "Starting Stable Diffusion Web UI"
     cd /workspace/stable-diffusion-webui
     source /workspace/venv/bin/activate
@@ -65,6 +81,13 @@ else
     nohup ./gui.sh --listen 0.0.0.0 --server_port 3011 --headless > /workspace/logs/kohya_ss.log 2>&1 &
     echo "Kohya_ss started"
     echo "Log file: /workspace/logs/kohya_ss.log"
+
+    echo "Starting ComfyUI"
+    cd /workspace/ComfyUI
+    source venv/bin/activate
+    python3 main.py --listen 0.0.0.0 --port 3021 > /workspace/logs/comfyui.log 2>&1 &
+    echo "ComfyUI started"
+    echo "Log file: /workspace/logs/comfyui.log"
 fi
 
 if [ ${ENABLE_TENSORBOARD} ];
@@ -82,18 +105,3 @@ then
 fi
 
 echo "All services have been started"
-
-if [ "${DOWNLOAD_SDXL}" == "1" ];
-then
-    # Only download the models if they have not already been downloaded previously
-    if [[ ! -e "/workspace/stable-diffusion-webui/models/Stable-diffusion/sd_xl_base_1.0.safetensors" ]];
-    then
-        echo "Beginning download of SDXL models"
-        /download_sdxl_models.sh
-        echo "SDXL model download complete"
-        echo "Beginning download of SDXL styles"
-        cd /workspace/stable-diffusion-webui
-        wget https://raw.githubusercontent.com/Douleb/SDXL-750-Styles-GPT4-/main/styles.csv
-        echo "SDXL styles download complete"
-    fi
-fi
