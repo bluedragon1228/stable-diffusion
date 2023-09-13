51fff485ee03c418cb6d86d7e9757ef379c3425e
bluedragon1228 <joeyharvey066@gmail.com>
2023-09-13 10:18:27 +0200

    Fix invalid symlinks

--- pre_start.sh
+++ pre_start.sh
@@ -39,8 +39,6 @@ echo "Fixing ComfyUI venv..."
 /fix_venv.sh /ComfyUI/venv /workspace/ComfyUI/venv
 
 # Link modelS and VAE
-ln -s /sd-models/v1-5-pruned.safetensors /workspace/stable-diffusion-webui/models/Stable-diffusion/v1-5-pruned.safetensors
-ln -s /sd-models/vae-ft-mse-840000-ema-pruned.safetensors workspace/stable-diffusion-webui/models/VAE/vae-ft-mse-840000-ema-pruned.safetensors
 ln -s /sd-models/sd_xl_base_1.0.safetensors /workspace/stable-diffusion-webui/models/Stable-diffusion/sd_xl_base_1.0.safetensors
 ln -s /sd-models/sd_xl_refiner_1.0.safetensors /workspace/stable-diffusion-webui/models/Stable-diffusion/sd_xl_refiner_1.0.safetensors
 ln -s /sd-models/sdxl_vae.safetensors /workspace/stable-diffusion-webui/models/VAE/sdxl_vae.safetensors
