fddfbccb18271761a95a082226784de6d6467c28
bluedragon1228 <joeyharvey066@gmail.com>
2024-01-19 09:06:21 +0200

    Support bin files for all of the venvs

--- scripts/fix_venv.sh
+++ scripts/fix_venv.sh
@@ -2,13 +2,15 @@
 
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <OLD_VENV> <NEW_VENV>"
-  echo "   eg: $0 /home/ubuntu/test/venv /home/ubuntu/test/venv2"
+  echo "   eg: $0 /venv /workspace/venv"
   exit 1
 fi
 
 OLD_PATH=${1}
 NEW_PATH=${2}
 
+echo "Fixing venv. Old Path: ${OLD_PATH}  New Path: $NEW_PATH}"
+
 cd ${NEW_PATH}/bin
 sed -i "s|VIRTUAL_ENV=\"${OLD_PATH}\"|VIRTUAL_ENV=\"${NEW_PATH}\"|" activate
-sed -i 's|#!/venv/bin/python3|#!/workspace/venv/bin/python3|' *
+sed -i "s|#\!${OLD_PATH}/bin/python3|#\!${NEW_PATH}/bin/python3|" *
