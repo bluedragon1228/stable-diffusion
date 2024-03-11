9c7faa391d2cbcc68e1baf8bc89b323076c7502c
bluedragon1228 <joeyharvey066@gmail.com>
2024-03-11 12:43:43 +0200

    Updated Kohya start script to set HF_HOME

--- scripts/start_kohya.sh
+++ scripts/start_kohya.sh
@@ -1,5 +1,6 @@
 echo "Starting Kohya_ss Web UI"
 cd /workspace/kohya_ss
+export HF_HOME="/workspace"
 nohup ./gui.sh --listen 0.0.0.0 --server_port 3011 --headless > /workspace/logs/kohya_ss.log 2>&1 &
 echo "Kohya_ss started"
 echo "Log file: /workspace/logs/kohya_ss.log"
